530a8eaf09543eff90741e4568fe1675dbb8759e
Himanshu <er.himanshuk.00@gmail.com>
2026-02-19 01:27:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const addTask = document.getElementById("add-task")
+const taskList = document.getElementById("task-list")
+const textInput = document.getElementById("text-input")
+
+let allTask = []
+addTask.addEventListener("click", (e)=>{
+    e.preventDefault()
+    const task = textInput.value.trim()
+    if(task !==""){
+        let taskItem = document.createElement("object")
+        taskItem={
+            id: Date.now(),
+            text:task,
+        }
+        allTask.push(taskItem)
+    }
+    taskList.innerHTML=""
+    displayTask()
+})
+
+// rendering tasks
+function displayTask(){
+    allTask.forEach((task)=>{
+        const listItem = document.createElement("li")
+        listItem.innerHTML=`${task.text} <button id="${task.id}">edit</button> <button id="${task.id}">delete</button>`
+        taskList.append(listItem)
+        textInput.value=""
+    })
+}
+
+// deleting and editing tasks
+taskList.addEventListener("click", (e)=>{
+    if(e.target.tagName === "BUTTON" && e.target.textContent === "delete"){
+        const taskId = parseInt(e.target.id)
+        let items = allTask.filter((task) =>(task.id !== taskId))
+        allTask.length=0
+        allTask=[...items]
+        taskList.innerHTML=""
+        displayTask()    
+    }else if(e.target.tagName === "BUTTON" && e.target.textContent === "edit"){
+        const taskId = parseInt(e.target.id)
+       
+        
+    }
+    })
+
